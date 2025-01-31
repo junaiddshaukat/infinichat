d4f2966a0bafcd55474392151b4af5646ed9a7d5
NicolasGrimm <hello@nicolasgrimm.ch>
2025-01-31 20:53:57 +0500

    message api doen

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,19 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-
+import cookieParser from "cookie-parser"
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`🔨 Server is running on the port ${PORT}`);
