ad14ff2928359b968e0d83ad11c96e2040613396
NicolasGrimm <hello@nicolasgrimm.ch>
2025-02-02 19:55:55 +0500

    project completed

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,51 +1,49 @@
 import User from "../models/user.model.js";
-import Message from "../models/message.model.js"
+import Message from "../models/message.model.js";
+
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
-    const filteredUsers = await User.find({
-      _id: { $ne: loggedInUserId },
-    }).select("--password");
+    const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-res.status(200).json(filteredUsers)
-
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.log("Error in the getUserForSidebar : ", error.message);
-    res.status(500).json({error:"Internal Server error"})
-
+    console.error("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
-
-export const getMessages = async (req,res) => {
+export const getMessages = async (req, res) => {
   try {
-    const {id:userToChatId} = req.params
+    const { id: userToChatId } = req.params;
     const myId = req.user._id;
+
     const messages = await Message.find({
-      $or:[
-        {myId:myId,receiverId:userToChatId},
-        {myId:userToChatId,receiverId:myId},
-      ]
-    })
-    res.status(200).json(message)
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
   } catch (error) {
-    console.log("Error in the getMessages : ", error.message);
-    res.status(500).json({error:"Internal Server error"})
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
-}
+};
 
-export const sendMessage = async (req,res) =>{
-
+export const sendMessage = async (req, res) => {
   try {
-    const {text,image} = req.body;
-    const {id:recieverId} = req.params;
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
     let imageUrl;
-    if(image){
-      //Upload base64 Image to cloudinary
+    if (image) {
+      // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
     }
@@ -54,16 +52,19 @@ export const sendMessage = async (req,res) =>{
       senderId,
       receiverId,
       text,
-      image:imageUrl,
-    })
+      image: imageUrl,
+    });
 
     await newMessage.save();
 
-  // realtime functionality goes here -> socket.io
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
-  res.status(201).json(newMessage)
+    res.status(201).json(newMessage);
   } catch (error) {
-    console.log("Error in the message : ", error.message);
-    res.status(500).json({error:"Internal Server error"})
+    console.log("Error in sendMessage controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,18 +4,24 @@ import messageRoutes from "./routes/messageRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
-const app = express();
+import cors from "cors";
+import { app,server } from "./lib/socket.js";
+
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🔨 Server is running on the port ${PORT}`);
   connectDB();
 });
